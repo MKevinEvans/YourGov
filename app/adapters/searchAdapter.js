04935b3a108f137d4680838ee0661411850f0b0b
nikymorg <nicole.morgan@gmail.com>
2016-12-27 15:28:34 -0700

    Render articles

--- app/adapters/searchAdapter.js
+++ app/adapters/searchAdapter.js
@@ -4,7 +4,7 @@ function searchAdapter(repName){
   const formattedRepName = repName.split(" ").join("%20")
   new Search(repName)
   const searchURL = searchRootURL + formattedRepName + "&cx=" + SearchEngineId + "&num=5&key=" + SearchAPIKey
-    
+
     return $.ajax({
     method: "GET",
     url: searchURL
@@ -13,24 +13,27 @@ function searchAdapter(repName){
 
 
 
-function parseSearchResults(results){   
+function parseSearchResults(results){
   // NOTE: infoObjects is an array of objects with keys for 'title' and 'link'
   let infoObjects = results.items.map(function(result){return Object.assign({}, {title: result.title, link: result.link})})
-  return showLinks(infoObjects)
+  return showLinks(infoObjects, results.queries.request[0].searchTerms)
 }
 
 
 
-function showLinks(infoObjects){
-  
-  let display = (`<div>
-            <ul>
-  ${infoObjects.map((object) =>{
-    return ( `<li><a href="${object.link}">${object.title}</a></li>`)}
-            )}
-          </ul>
-          </div>`
-)
-  
-  return display
+function showLinks(infoObjects, repName){
+
+  let display = (
+    `<div>
+      <h2>${repName} Articles</h2>
+      <ul>
+        ${infoObjects.map((object) =>{
+          return ( `<li><a href="${object.link}">${object.title}</a></li>`)}
+        ).join("")}
+      </ul>
+    </div>`
+  )
+
+  $('#phone-disclaimer').empty()
+  $('#results').empty().append(display)
 }
